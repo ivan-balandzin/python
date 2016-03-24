99e83f0f0c0044edf4c862232aa625183d14ba4c
ivan-balandzin <ivan_balandzin@epam.com>
2016-03-24 14:03:45 +0000

    Update task2.py

--- Topic2/task2.py
+++ Topic2/task2.py
@@ -5,6 +5,6 @@ b = a.lower().replace(' ', '', len(a))
 # то же самое, но еще и слово в обратном порядке
 c = a.lower().replace(' ', '', len(a))[::-1]
 if b == c:
-	print ("The word is polyndrome")
+	print ("The word is palindrome")
 else:
-	print ("The word is not polyndrome")
+	print ("The word is not palindrome")
